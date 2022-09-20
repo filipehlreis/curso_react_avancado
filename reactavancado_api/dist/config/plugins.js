572bbbfd321ec75f52caa4f322e65e430bb8c282
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-19 22:56:53 -0300

    instalando grapql

---
+++ reactavancado_api/dist/config/plugins.js
@@ -0,0 +1,18 @@
+"use strict";
+// path: ./config/plugins.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    //
+    graphql: {
+        config: {
+            endpoint: '/graphql',
+            shadowCRUD: true,
+            playgroundAlways: false,
+            depthLimit: 7,
+            amountLimit: 100,
+            apolloServer: {
+                tracing: false,
+            },
+        },
+    },
+};
