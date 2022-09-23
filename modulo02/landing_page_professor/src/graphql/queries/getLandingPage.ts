8ad1cb379b6ee18c2b0433a9be8e98d4f82df89a
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-22 21:47:13 -0300

    teste

---
+++ modulo02/landing_page_professor/src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,19 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  query GET_LANDING_PAGE {
+    landingPage {
+      data {
+        attributes {
+          logo {
+            data {
+              attributes {
+                alternativeText
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default GET_LANDING_PAGE
